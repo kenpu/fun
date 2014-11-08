55e420efcc626b0970b09b355483dc04df3221c1
kenpu <kenpuca@gmail.com>
2014-11-07 21:10:13 -0500

    alpha version

--- yonge-bd-2014/pokemon.py
+++ yonge-bd-2014/pokemon.py
@@ -1,6 +1,9 @@
 import cv
 import pygame
 import sys
+import os
+from time import time, sleep
+import subprocess
 
 #
 # Initialize CV
@@ -28,19 +31,35 @@ def center(surf0, surf1, x=None, y=None):
 # Initialize PyGame display
 #
 pygame.init()
-size = width, height = 1366, 744
+size = width, height = 1024, 600
 screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
 
 _quit = False
 
+# load image files
+
+image_files = [f for f in os.listdir(".") if f.endswith(".png")]
+image_files.extend(f for f in os.listdir(".") if f.endswith(".jpg"))
+image_files.sort()
+
+if not image_files:
+  print "ERROR: no background files found."
+  sys.exit(0)
+
 # load the card
+
 card_width = int(height / 300.0 * 215.)
 card_height = height
-card_surf  = pygame.Surface((card_width, card_height))
-card_image = pygame.image.load("card0.jpg")
-card_surf.blit(pygame.transform.smoothscale(
-    card_image, (card_width, card_height)), (0,0))
 
+def load_card(i=0):
+  card_surf  = pygame.Surface((card_width, card_height))
+  card_image = pygame.image.load(image_files[i])
+  card_surf.blit(pygame.transform.smoothscale(
+      card_image, (card_width, card_height)), (0,0))
+  return card_surf
+
+image_idx = 0
+card_surf = load_card(image_idx)
 
 # the window in the capture frame
 capture_width_crop = 640
@@ -52,21 +71,33 @@ face_height = int(float(face_width) / capture_width_crop * capture_height_crop)
 face_surf  = pygame.Surface((face_width, face_height))
 # face_surf.set_alpha(255)
 
-# build a head mask
-mask_surf = pygame.Surface((face_width, face_height))
-pygame.draw.ellipse(mask_surf, 
-    (0,0,0), (0, 0, face_width, face_height))
-mask = pygame.mask.from_surface(mask_surf)
-
 # Merged face + card
 merge_surf = pygame.Surface((card_width, card_height))
+
+def print_image():
+  try:
+    filename = "output/%s.png" % str(time())
+    pygame.image.save(merge_surf, filename)
+    print "image saved:", filename
+    subprocess.call(["/usr/bin/lp", "-o", "scaling=60", filename])
+    sleep(30.0)
+  except Exception, e:
+    print e
+
 
 while True:
     for e in pygame.event.get():
         if e.type is pygame.QUIT:
             _quit = True
         if e.type is pygame.KEYDOWN:
+          if e.key == pygame.K_ESCAPE:
             _quit = True
+          if e.key == pygame.K_SPACE or e.key == pygame.K_PAGEUP:
+            image_idx = (image_idx + 1) % len(image_files)
+            card_surf = load_card(image_idx)
+          if e.key == pygame.K_PAGEDOWN:
+            print_image()
+
 
     if _quit: break
 
@@ -94,7 +125,8 @@ while True:
 
     pygame.display.flip()
 
-pygame.image.save(merge_surf, "surf0.png")
+
+
 
 pygame.display.quit()
 del cam
